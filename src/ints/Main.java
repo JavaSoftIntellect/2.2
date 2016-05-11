37a7df36ce7faf157f1822c8d4a7c0f1e97a5cab
Remzi <remzitdf@abv.bg>
2016-05-11 18:42:03 +0300

    ints

---
+++ src/ints/Main.java
@@ -0,0 +1,18 @@
+package ints;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 8;
+		int c;
+		c=a; a=b; b=c;
+		System.out.print("a:");
+		System.out.println(a);
+		System.out.print("b:");
+		System.out.println(b);
+		
+
+	}
+
+}
